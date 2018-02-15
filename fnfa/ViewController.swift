fc031abd0f8bce2bdf74ed3f4f4de843a9c8601c
Omar Jbara <omar.jbara2@gmail.com>
2018-02-15 14:46:27 +0100

    Prepare for structure

---
+++ fnfa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fnfa
+//
+//  Created by JBARA Omar on 15/02/2018.
+//  Copyright © 2018 JBARA Omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

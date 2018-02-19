a7fbe83d56bc1ffa6c9b5e5268d7ba0601beea0f
Omar Jbara <omar.Jbara2@gmail.com>
2018-02-19 02:34:16 +0100

    - Reset image in events.json - Added label for place and duration in EventCollectionViewCell

--- fnfa/Agenda/AgendaTableViewCell.swift
+++ fnfa/Agenda/AgendaTableViewCell.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import UPCarouselFlowLayout
 
 class AgendaTableViewCell: UITableViewCell {
 
     @IBOutlet weak var eventsCollectionView: UICollectionView!
-
+    @IBOutlet weak var eventsCollectionFlowLayout: UPCarouselFlowLayout!
+    
     var eventsDataSource: EventsDataSource?
     var day: Date? {
         didSet {
@@ -23,6 +25,7 @@ class AgendaTableViewCell: UITableViewCell {
     private func setup() {
         self.eventsDataSource = EventsDataSource(events: DataMapper.instance.events.findBy(date: day!)!)
         dateFormatter.setLocalizedDateFormatFromTemplate("d")
+        eventsCollectionFlowLayout.spacingMode = .overlap(visibleOffset: 30)
         eventsCollectionView.dataSource = eventsDataSource
         eventsCollectionView.reloadData()
         eventsCollectionView.register(UINib(nibName: String(describing: EventCollectionViewCell.self), bundle: nil),
--- fnfa/Agenda/EventCollectionViewCell.swift
+++ fnfa/Agenda/EventCollectionViewCell.swift
@@ -11,12 +11,23 @@ import UIKit
 class EventCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var labelExcerpt: UILabel!
     @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelPlace: UILabel!
+    @IBOutlet weak var labelDuration: UILabel!
     
     var event: Event? {
         didSet{
+            let df = DateFormatter()
+            df.locale = Locale(identifier: "fr_FR")
+            df.setLocalizedDateFormatFromTemplate("HH'h'mm")
+            let start = df.string(from: (event?.startingDate.getDate())!)
+            let end = df.string(from: (event?.endingDate.getDate())!)
             eventImage.image = event?.getUIImage()
             labelTitle.text = event?.name
+            labelExcerpt.text = event?.excerpt
+            labelPlace.text = event?.places?.first?.name
+            labelDuration.text = "De \(start) à \(end)"
         }
     }
 
